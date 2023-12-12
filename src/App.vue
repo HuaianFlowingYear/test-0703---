a916eb5c1ff60a89639661c6484fe896273dac28
Kmihuyou <1807050330@qq.com>
2023-12-12 11:25:01 +0800

    version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your 李帅" />
+    <HelloWorld msg="Welcome to Your 孟宗坤" />
   </div>
 </template>
 
