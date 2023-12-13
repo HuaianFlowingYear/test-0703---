7643758da3316d1702aecd4108da959aa347eabf
amicg <1748425207@qq.com>
2023-12-13 11:27:05 +0800

    中间内容

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
     <h1 class="bg">这是完美无BUG低功耗优美UI的超级-- 功能1(张傲宇)</h1>
     <p>这是关于我给张宝其的自我介绍,我叫张宝其，我是个秒男,八秒真男人,第九秒就不行了(王家轩)</p>
     <h3>我叫张宝奇</h3>
+    <div class="miffle_Tew">
+        <h3>招商面板</h3>
+        <p>这是页面中间内容第三块</p>
+    </div>
   </div>
 </template>
 
@@ -33,5 +37,18 @@ export default {
 .bg {
   color: yellow;
   background: brown;
+}
+.miffle_Tew {
+  background: #fff;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.miffle_Tew h3 {
+  background: #000;
+  color: #fff;
+  padding: 5px;
+  margin: 0;
 }
 </style>
