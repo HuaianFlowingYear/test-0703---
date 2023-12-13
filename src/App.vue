b3c1710f69bb2cc1fed6c978675361fd620cd5fe
zhangbaoqi03 <1603971193@qq.com>
2023-12-13 11:56:22 +0800

    bottom

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,9 @@
     <p>这是关于我给张宝其的自我介绍,我叫张宝其，我是个秒男,八秒真男人,第九秒就不行了(王家轩)</p>
     
     <h3>我叫张宝奇</h3>
-<<<<<<< HEAD
     <div class="add">
       <button @click="dianji">我是中间的第三个div,请点我(王家轩)</button>
     </div>
-=======
     <section>
       <div id="one" class="bg-[red] h-[15vw]">
         中间的第一个div
@@ -26,7 +24,6 @@
 
     <div class="add">
       <button @click="dianji">我是中间的第三个div,请点我(王家轩)</button>
->>>>>>> 32bd18823d29c6ad2222893e7e4ee3a00e281bbe
     <div class="miffle_Tew">
         <h3>招商面板</h3>
         <p>这是页面中间内容第三块</p>
